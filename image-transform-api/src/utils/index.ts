4421fc100c2290aa8023df15cbb5f9e4bdf718d1
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-04-19 11:54:43 +0400

    :white_check_mark: Pass Tests

--- image-transform-api/src/utils/index.ts
+++ image-transform-api/src/utils/index.ts
@@ -1,2 +1,3 @@
 export { getPath } from './getPath.js';
+export { getRatio, getSize } from './getSize.js';
 export { processImage } from './processImage.js';
--- image-transform-api/src/utils/processImage.ts
+++ image-transform-api/src/utils/processImage.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import { ImagePool } from '@squoosh/lib';
 import { cpus } from 'os';
+import { getSize } from './getSize.js';
 
 const encodeOptions = {
   mozjpeg: 'auto',
@@ -8,18 +9,33 @@ const encodeOptions = {
 
 export const processImage = async (
   file: Buffer,
-  options: { width?: number; height?: number }
+  options: { width: number; height: number; [key: string]: any }
 ) => {
+  const orig = getSize(file) as { [key: string]: any };
+  ['width', 'height'].forEach((key) => {
+    options[key] =
+      options[key] <= 0 || options[key] === Infinity
+        ? 0
+        : options[key] >= orig[key]
+        ? orig[key]
+        : options[key];
+  });
+  if (options.width === orig.width && options.height === orig.height) {
+    return file;
+  }
   const pool = new ImagePool(cpus().length);
   const image = pool.ingestImage(file);
   if (options.width || options.height) {
-    const preprocessOptions = {
-      resize: options,
+    const preprocessOptions: {
+      resize: { width: number; height: number; fitMethod: string };
+    } = {
+      resize: { ...options, fitMethod: 'contain' }, // seems @squoosh/lib doesn't yet support fitMethod
     };
     await image.preprocess(preprocessOptions);
   }
   await image.encode(encodeOptions);
   await pool.close();
+
   const result = await image.encodedWith.mozjpeg;
   return result.binary;
 };
