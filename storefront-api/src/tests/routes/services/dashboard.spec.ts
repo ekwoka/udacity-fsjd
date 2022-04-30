0413510e2643a3b0b93d81c22699cf57252327d5
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-04-30 13:35:55 +0400

    :test_tube: Tests /services endpoints
    /most_expensive
    /active_users

---
+++ storefront-api/src/tests/routes/services/dashboard.spec.ts
@@ -0,0 +1,19 @@
+import supertest from 'supertest';
+import { app } from '../../../server';
+
+describe('/services routes', () => {
+  it('GET /most_expensive', async () => {
+    const { status, body } = await supertest(app).get(
+      '/services/most_expensive'
+    );
+    expect(status).toBe(200);
+    expect(Array.isArray(body)).toBeTrue();
+    expect(body[0].price).toBeDefined();
+  });
+  it('GET /active_users', async () => {
+    const { status, body } = await supertest(app).get('/services/active_users');
+    expect(status).toBe(200);
+    expect(Array.isArray(body)).toBeTrue();
+    expect(body[0].user_id).toBeDefined();
+  });
+});
