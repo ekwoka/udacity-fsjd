7c9fc9554488dd637f4b18c2c0ff7db4bd85d69b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-05-05 14:18:35 +0400

    :card_file_box: Add Default Price and optional category

--- storefront-api/migrations/sqls/20220505073240-products-table-up.sql
+++ storefront-api/migrations/sqls/20220505073240-products-table-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  category_id INTEGER NOT NULL,
+  price DECIMAL(10,2) NOT NULL default '0.00',
+  category_id INTEGER,
   FOREIGN KEY (category_id) REFERENCES categories(id)
 );
