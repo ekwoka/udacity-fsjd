bc6270370a315a4c16de3ca35ce93052ab105fee
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-05-05 16:24:14 +0400

    :test_tubes: Tests for limiting orders to current user

--- storefront-api/src/tests/routes/orders.spec.ts
+++ storefront-api/src/tests/routes/orders.spec.ts
@@ -9,23 +9,31 @@ import {
   UserStore,
 } from '../../models';
 import { database } from '../helpers/databaseSetup';
+import { createJWT } from '../../utils';
 
 describe('/orders Route', () => {
   let testOrders: Order[];
   let testProduct: ProductDB;
   let testUser: UserReturn;
+  let token: string;
   beforeAll(async () => {
     await database();
     testUser = (await UserStore.index())[0];
     testProduct = (await ProductStore.index())[0];
     testOrders = await OrderStore.index();
+    token = await createJWT(testUser);
   });
-  it('GET / should return array of orders', async () => {
-    const { status, body } = await supertest(app).get('/orders');
+  it('GET / should return orders from user', async () => {
+    const { status, body } = await supertest(app)
+      .get('/orders')
+      .set('Authorization', `Bearer ${token}`);
     expect(status).toBe(200);
     expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toEqual(
+      testOrders.filter((order) => order.user_id === testUser.id).length
+    );
   });
-  it('GET /:id returns an order', async () => {
+  it('GET /:id returns a specific order', async () => {
     const { id } = testOrders[0];
     const { status, body } = await supertest(app).get(`/orders/${id}`);
     expect(status).toBe(200);
