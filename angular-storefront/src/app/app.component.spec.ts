a077353b56debb70abcdcb0d5727872636c348d1
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-05-09 07:04:25 +0400

    :test_tube: Update Tests

--- angular-storefront/src/app/app.component.spec.ts
+++ angular-storefront/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -23,13 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-storefront'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-storefront');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-storefront app is running!');
+    expect(app.title).toEqual('My Store');
   });
 });
