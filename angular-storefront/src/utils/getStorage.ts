923007dd45c05616864c705cba4d2f1dce1298ef
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-05-09 17:23:24 +0400

    :sparkles: Implements Storage Proxy

---
+++ angular-storefront/src/utils/getStorage.ts
@@ -0,0 +1,33 @@
+export const getStorage = <T>(
+  prefix: string,
+  initial: T,
+  override: boolean = false
+): T => {
+  const target: ProxyBase = Array.isArray(initial) ? [] : {};
+  const update = () => localStorage.setItem(prefix, JSON.stringify(target));
+  const proxyOutput = wrapProxy(target, update) as ProxyBase;
+  const itemInStorage = localStorage.getItem(prefix);
+  if (override) return Object.assign(proxyOutput, initial) as T;
+  if (itemInStorage)
+    return Object.assign(proxyOutput, JSON.parse(itemInStorage)) as T;
+  return proxyOutput as T;
+};
+
+const wrapProxy = (item: any, update: () => void) => {
+  const proxyOutput = new Proxy(item, {
+    set: (target: ProxyBase, key: string, value) => {
+      target[key] = value;
+      if (value && typeof value === 'object') {
+        target[key] = wrapProxy(value, update);
+      }
+      update();
+      return true;
+    },
+  });
+
+  return Object.assign(proxyOutput, item);
+};
+
+type ProxyBase = {
+  [key: string]: any;
+};
