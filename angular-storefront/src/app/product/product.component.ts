f30a479eb1c7192231aac9cde96d7f8d3141d540
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-05-09 07:09:46 +0400

    :construction: Adds product component for collections

--- angular-storefront/src/app/product/product.component.ts
+++ angular-storefront/src/app/product/product.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../products.service';
 
 @Component({
   selector: 'storefront-product',
-  templateUrl: './product.component.html'
+  template: `
+    <div
+      class="flex flex-col items-center justify-center gap-4 p-8"
+      id="product-{{ product.id }}">
+      <img
+        class="h-auto w-full rounded"
+        src="https://placekitten.com/{{ 300 + product.id * 12 }}/{{
+          300 + product.id * 12
+        }}"
+        width="1"
+        height="1"
+        alt="{{ product.name }}" />
+      <h2 class="text-xl">{{ product.name }}</h2>
+      <p class="text-2xl font-semibold">$ {{ product.price }}</p>
+      <button-primary
+        [text]="'add to cart'"
+        [onclick]="clickHandler"></button-primary>
+    </div>
+  `,
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() product: Product;
+  clickHandler = (e: Event): void => {
+    this.cartService.addToCart(this.product);
+  };
+  constructor(private cartService: CartService) {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0,
+    };
   }
 
+  ngOnInit(): void {}
 }
